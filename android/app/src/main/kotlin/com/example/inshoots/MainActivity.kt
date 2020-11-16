675aa2de7f44795375b9feba9c8bfd5a2a53b36b
Arteev Raina <arteevraina@gmail.com>
2020-11-16 20:20:28 +0530

    feat: initialized flutter project

---
+++ android/app/src/main/kotlin/com/example/inshoots/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inshoots
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:inshoots/views/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
